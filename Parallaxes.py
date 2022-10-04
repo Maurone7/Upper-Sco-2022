29b6b4252f5a219aceb59e643613647b54ccc78d
Mauro Patimo <mauro.patimo@gmail.com>
2022-10-04 00:29:56 -0700

    Updates
    Updated luminosity error values

--- Parallaxes.py
+++ Parallaxes.py
@@ -1,7 +1,6 @@
 from data_file import luminosity_list_barenfeld, luminosity_list_barenfeld_error, temperature_list_barenfeld,\
     temperature_list_barenfeld_error, temperature_feiden, mass_feiden, source_list_garrett, source_list_barenfeld_2016
 
-from statistics import mean
 from scipy.interpolate import interp1d
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,6 +11,7 @@ plt.setp(ax.spines.values(), linewidth=2)
 
 #avoid axis labels being cut
 plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
+
 
 #import values for all sources into a table
 value_single_source = []
@@ -62,12 +62,9 @@ parallax_106 = ['6.896551724' if item == 'null' else item for item in parallax_1
 parallax_error_106 = ['0.83594566' if item == 'null' else item for item in parallax_error_106]
 for index_ruwe, element_ruwe in enumerate(ruwe_list):
     if float(element_ruwe) > 1.6:
-        print(sources_106[index_ruwe])
-        print(element_ruwe)
         parallax_106[index_ruwe] = '6.896551724'
         parallax_error_106[index_ruwe] = '0.83594566'
 
-print(parallax_106[sources_106.index('J16020287-2236139')])
 #change elements to floats
 parallax_106 = [float(x) for x in parallax_106]
 parallax_error_106 = [float(x) for x in parallax_error_106]
@@ -82,6 +79,7 @@ for x in range(len(parallax_106)):
     distances_lower_error_106.append((1 / (parallax_106[x] + parallax_error_106[x])) * 1000 - distances_106[x])
 
 distances_lower_error_106 = [-20.0 if round(x, 2) == -15.68 else x for x in distances_lower_error_106]
+
 #update luminosity and the error
 luminosity_106 = []
 luminosity_error_106 = []
@@ -108,43 +106,51 @@ for index_source_garrett, element_source_garrett in enumerate(source_list_garret
             distances_updated_upper_sco.append(distances_106[index_source_barenfeld])
 
 
-def update_luminosity(old_luminosity_list, new_distance):
-    return (10**old_luminosity_list) * (new_distance**2) / (145**2)
-
-
-# create function for creating error in luminosity
-def update_luminosity_error(distance_new, new_distance_error, old_luminosity, old_luminosity_error):
-    return np.sqrt(((distance_new/145)**2 * old_luminosity_error)**2 + (old_luminosity/(145**2) * (2*distance_new) * new_distance_error)**2)
-
-
 luminosity_upper_error_106 = []
 luminosity_lower_error_106 = []
 luminosity_106 = [10**x for x in luminosity_106]
 
-# create list with luminoisties error NOT in log scale
-luminosity_upper_error = []
-luminosity_lower_error = []
+# update luminoisity with new distances
+def update_luminosity(old_luminosity, new_distance):
+    return old_luminosity * (new_distance**2) / (145**2)
 
-for x in range(106):
-    luminosity_upper_error.append(10**(luminosity_list_barenfeld[x] + luminosity_list_barenfeld_error[x]) - 10**luminosity_list_barenfeld[x])
-    luminosity_lower_error.append(10**(luminosity_list_barenfeld[x] - luminosity_list_barenfeld_error[x]) - 10**luminosity_list_barenfeld[x])
 
-for x in range(106):
-    luminosity_lower_error_106.append(update_luminosity_error(distances_106[x], distances_lower_error_106[x],
-                                                              luminosity_list_barenfeld[x], luminosity_lower_error[x]))
-    luminosity_upper_error_106.append(update_luminosity_error(distances_106[x], distances_upper_error_106[x],
-                                                              luminosity_list_barenfeld[x], luminosity_upper_error[x]))
+# update old error
+def update_old_luminosity_error(old_luminosity_error, old_luminosity):
+    return np.sqrt((old_luminosity_error**2) - (((2*old_luminosity/145) * 20) ** 2))
 
-luminosity_upper_error_1 = []
-luminosity_lower_error_1 = []
 
-#calculate in the error in log scale
-for x in range(106):
-    luminosity_upper_error_1.append(np.log10(luminosity_106[x] + luminosity_upper_error_106[x]) - np.log10(luminosity_106[x]))
-    luminosity_lower_error_1.append(np.log10(luminosity_106[x] + luminosity_lower_error_106[x]) - np.log10(luminosity_106[x]))
+# create function for creating error in luminosity
+def update_new_luminosity_error(distance_new, new_distance_error, old_luminosity, old_luminosity_error):
+    return np.sqrt(((distance_new/145)**2 * old_luminosity_error)**2 + (old_luminosity/(145**2) * (2*distance_new) * new_distance_error)**2)
 
-#get rid of log scale in updated luminosities values
-luminosity_106 = [np.log10(x) for x in luminosity_106]
+
+luminosity_list_barenfeld_error_NOT_log = []
+updated_luminosity_list_NOT_log = []
+luminosity_updated_old_error_NOT_log = []
+
+
+# calculate the luminosity NOT in log scale
+luminosity_list_barenfeld_NOT_log = [10**x for x in luminosity_list_barenfeld]
+
+
+for index, element in enumerate(luminosity_list_barenfeld_NOT_log):
+
+    # calculate new Barenfeld luminosity with new distances
+    updated_luminosity_list_NOT_log.append(update_luminosity(element, distances_106[index]))
+
+    # calculate Barenfeld luminosity error NOT in log scale
+    luminosity_list_barenfeld_error_NOT_log.append(10**(np.log10(element) + luminosity_list_barenfeld_error[index]) - element)
+    luminosity_updated_old_error_NOT_log.append(update_old_luminosity_error(luminosity_list_barenfeld_error_NOT_log[index], element))
+
+
+
+new_luminosity_error_log = []
+new_luminosity_error_NOT_log = []
+for index, element in enumerate(luminosity_list_barenfeld_NOT_log):
+    new_luminosity_error_NOT_log.append(update_new_luminosity_error(distances_106[index], distances_lower_error_106[index], element, luminosity_updated_old_error_NOT_log[index]))
+    new_luminosity_error_log.append(np.log10(new_luminosity_error_NOT_log[index] + luminosity_list_barenfeld_NOT_log[index]) - np.log10(luminosity_list_barenfeld_NOT_log[index]))
+
 
 #manipulate trasnparency errobars
 kwargs_errobar = {'alpha': 0.3}
@@ -156,7 +162,7 @@ y = [-2, 1]
 
 plt.scatter(luminosity_list_barenfeld, luminosity_106)
 plt.errorbar(luminosity_list_barenfeld, luminosity_106, xerr=luminosity_list_barenfeld_error, ls='none', c='gray', **kwargs_errobar)
-plt.errorbar(luminosity_list_barenfeld, luminosity_106, yerr=(luminosity_upper_error_1, luminosity_lower_error_1), ls='none', c='gray', **kwargs_errobar)
+plt.errorbar(luminosity_list_barenfeld, luminosity_106, yerr=(new_luminosity_error_log, new_luminosity_error_log), ls='none', c='gray', **kwargs_errobar)
 plt.xlabel('$L_{Barenfeld} log(L_{\star}/L_{\odot})$', fontsize=20), plt.ylabel('$L_{new} log(L_{\star}/L_{\odot})$', fontsize=20)
 plt.grid()
 
@@ -184,6 +190,3 @@ plt.grid()
 ax.tick_params(which='both', labelsize=15)
 plt.savefig('Mass 106 Feiden')
 plt.show()
-
-print(distances_106)
-print(luminosity_list_barenfeld)
