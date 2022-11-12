74aa99e9ff746b54a49557a91f24df0127170719
Mauro Patimo <mauro.patimo@gmail.com>
2022-11-12 12:11:10 -0800

    Updates

--- Gradient_age_in_HR_diagram.py
+++ Gradient_age_in_HR_diagram.py
@@ -44,13 +44,7 @@ x = np.linspace(-1.5, 1)
 
 matching_temperatures_barenfeld_garrett = [10 ** x for x in matching_temperatures_barenfeld_garrett]
 
-z = np.polyfit(luminosity_list_barenfeld_updated, spectral_index_upper_sco, 1)
-p = np.poly1d(z)
-plt.plot(x, p(x))
-#plt.fill_between(x, p(x), p(x)+0.3, **kwargs_errobar)
-#plt.fill_between(x, p(x), p(x)-0.3, **kwargs_errobar)
-
-#scatter T vs alpha
+#scatter L vs alpha
 plt.scatter(luminosity_list_barenfeld_updated, spectral_index_upper_sco, c=matching_temperatures_barenfeld_garrett, cmap='gray', zorder=2, label='Upper-Sco')
 plt.gca().invert_xaxis()
 plt.legend()
@@ -59,19 +53,10 @@ plt.xlabel(r"$L [L_{\odot}]$")
 plt.ylabel(r'$\alpha$', fontsize=20)
 plt.show()
 
-z = np.polyfit(ricci_luminosity_taurus_1mm_LOG, spectral_indices_taurus, 1)
-p_1 = np.poly1d(z)
+import seaborn as sns
+sns.regplot(x = )
+
 plt.gca().invert_xaxis()
-plt.plot(x, p_1(x))
-#plt.fill_between(x, p_1(x), p_1(x)+0.3, **kwargs_errobar)
-#plt.fill_between(x, p_1(x), p_1(x)-0.3, **kwargs_errobar)
 plt.scatter(ricci_luminosity_taurus_1mm_LOG, spectral_indices_taurus, c=ricci_temperature_taurus_1mm)
 plt.xlabel(r"$L [L_{\odot}]$", fontsize=20), plt.ylabel(r'$\alpha$', fontsize=20)
 plt.show()
-
-plt.xlabel(r"$L [L_{\odot}]$", fontsize=20), plt.ylabel(r'$\alpha$', fontsize=20)
-plt.plot(x, p(x))
-plt.plot(x, p_1(x))
-plt.show()
-print("Upper-Sco", scipy.stats.linregress(x, p(x)))
-print("Taurus", scipy.stats.linregress(x, p_1(x)))
