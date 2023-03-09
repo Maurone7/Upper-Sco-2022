c828e78d749a69812f15becf07010338a055306e
Mauro Patimo <mauro.patimo@gmail.com>
2023-03-09 15:04:15 -0800

    Updates

--- Table_1.py
+++ Table_1.py
@@ -32,7 +32,7 @@ for index_source_garrett, element_source_garrett in enumerate(source_list_garret
             # luminosity_list_patimo_upper_error.append(upper_error_new_luminosities_106[index_source_barenfeld])
             # luminosity_list_patimo_lower_error.append(lower_error_new_luminosities_106[index_source_barenfeld])
 
-with open('Table 1.txt', 'w') as f:
+with open('Table 1.tex', 'w') as f:
     f.write(tabulate(table, headers=['Sources', 'SpT', r'$log(T_{\rm{eff}}/\rm{K})$', 'Distance',
                                      'log frac{l_{\star}}{L_{\odot}}', 'Stellar mass (M_{\star})']))
 
